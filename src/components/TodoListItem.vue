13e201657d91535198676b5fdeb2a0460707d56b
bijukb <bijukb7233@gmail.com>
2024-11-21 14:36:40 +0530

    initial setup

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { defineProps, watch } from "vue";
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+  },
+});
+const emit = defineEmits(["close", "complete","get"]);
+const deleteItem = () => {
+  emit("close", props.todo.id);
+};
+const completeItem = () => {
+  emit("complete", props.todo.id);
+  console.log(props.todo.completed);
+};
+const getItem=()=>{
+   emit("get",props.todo.id)
+}
+</script>
+
+<template>
+  <article class="container">
+  <div id="listItemContainer">
+   <span id="listItem" :style="`text-decoration:${todo.completed ? 'line-through red' : 'none'}`">{{ todo.item }}</span> 
+    <button class="itemBtn" @click="deleteItem">Close</button>
+    <button @click="getItem" :disabled="todo.completed" id="disabledBtn">Update</button>
+    <button class="itemBtn" @click="completeItem" >Complete</button>
+  </div>
+</article>
+</template>
+<style>
+.container{
+  width: max-content;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 10px;
+  background-color: rgb(82, 82, 150);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+ margin-bottom: 20px;
+}
+.container:hover{
+  background-color: blue;
+}
+#listItemContainer{
+			height: 5rem;
+			margin-top: 8px;
+      border-bottom: 1px solid yellowgreen;
+}
+#listItem{
+  /* overflow: hidden;
+  text-overflow: clip; */
+  font-size: xx-large;
+  color : rgb(90, 235, 90);
+  width: 200px; 
+  display: inline-block;
+}
+/* #listItem:hover{
+  overflow:  visible;
+} */
+button{
+  margin-right: 10px;
+}
+.itemBtn,#disabledBtn{
+  border: 1px solid white;
+}
+.itemBtn:disabled :active{
+  background-color: grey;
+} 
+.itemBtn:active{
+  color: blue;;
+}
+button:disabled{
+  background-color: grey;
+  border: none;
+}
+/* button[disabled]:focus{
+  outline:none;
+} */
+.itemBtn,#disabledBtn{
+  box-shadow: 0px 0px 1px 1px rgb(207, 140, 14);
+}
+
+</style>
